f32e8ce7a421472dabba6fe474617ea7d2cd99aa
Meown <gergon1999@gmail.com>
2026-01-12 13:54:42 +0300

    player movement and rotation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,44 @@
 using UnityEngine;
+using Fusion;
+using UnityEngine.InputSystem;
 
-public class Player : MonoBehaviour
+public class Player : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private CharacterController _controller;
+
+    private InputSystem_Actions _input;
+    private float PlayerSpeed = 10f;
+    private Plane _plane = new Plane(Vector3.up, Vector3.zero);
+
+    private void Awake()
     {
-        
+        _input = new();
+        _input.Player.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void FixedUpdateNetwork()
     {
-        
+        var input = _input.Player.Move.ReadValue<Vector2>();
+
+        Vector3 cameraForward = Camera.main.transform.up;
+        Vector3 cameraRight = Camera.main.transform.right;
+            
+        cameraForward.y = 0f;
+        cameraRight.y = 0f;
+        cameraForward.Normalize();
+        cameraRight.Normalize();
+            
+        var moveRelativeToCamera = cameraForward * input.y + cameraRight * input.x;
+
+        var look = _input.Player.ScreenPoint.ReadValue<Vector2>();
+        Debug.LogError(look);
+        var mouseRay = Camera.main.ScreenPointToRay(look);
+        var hit = _plane.Raycast(mouseRay, out var distance);
+        if (hit)
+        {
+            var point = mouseRay.GetPoint(distance);
+            _controller.transform.LookAt(point);
+        }
+        _controller.Move(moveRelativeToCamera * Runner.DeltaTime * PlayerSpeed);
     }
 }
